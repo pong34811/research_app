08db0f35782d8cbe8be9c2ed709f56beb9c4e17f
pong34811 <pong34811@gmail.com>
2024-08-24 01:31:53 +0700

    remove data file

--- Data/myshop/shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('#add-shop', views.add_shop, name='add_shop'),
-    path('shop-list/', views.shop_list, name='shop_list'),
-]
--- Data/research_management/projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.project_list, name='project_list'),
-    path('add/', views.project_add, name='project_add'),
-    path('tag/', views.add_event, name='add_event'),
-]
